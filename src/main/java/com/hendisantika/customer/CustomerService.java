917104ccc127a36326f4caf2ffcc39443271bfca
Hendi Santika <hendisantika@gmail.com>
2021-10-16 06:17:10 +0700

    #13 - Add save in Customer Service

--- src/main/java/com/hendisantika/customer/CustomerService.java
+++ src/main/java/com/hendisantika/customer/CustomerService.java
@@ -3,6 +3,7 @@ package com.hendisantika.customer;
 import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -39,5 +40,11 @@ public class CustomerService {
         return customerRepository.findByIdOptional(customerId).map(customerMapper::toDomain);
     }
 
+    @Transactional
+    public Customer save(Customer customer) {
+        CustomerEntity entity = customerMapper.toEntity(customer);
+        customerRepository.persist(entity);
+        return customerMapper.toDomain(entity);
+    }
 
 }
