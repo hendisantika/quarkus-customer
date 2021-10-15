993c2fb2045a67d227722f1bdfc720adadd10375
Hendi Santika <hendisantika@gmail.com>
2021-10-16 06:43:23 +0700

    #23 - Add CustomerResourceTest

---
+++ src/test/java/com/hendisantika/customer/CustomerResourceTest.java
@@ -0,0 +1,17 @@
+package com.hendisantika.customer;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-customer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/10/21
+ * Time: 06.42
+ */
+@QuarkusTest
+class CustomerResourceTest {
+
+}
