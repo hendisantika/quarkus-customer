db589628443fdca43a1b8e467559bb0a2f974494
Hendi Santika <hendisantika@gmail.com>
2021-10-16 07:08:14 +0700

    #32 - Add getHealth in HealthCheckTest

--- src/test/java/com/hendisantika/HealthCheckTest.java
+++ src/test/java/com/hendisantika/HealthCheckTest.java
@@ -1,6 +1,10 @@
 package com.hendisantika;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +17,12 @@ import io.quarkus.test.junit.QuarkusTest;
  */
 @QuarkusTest
 public class HealthCheckTest {
+    @Test
+    public void getHealth() {
+        given()
+                .when().get("/health")
+                .then()
+                .statusCode(200)
+                .body("status", Matchers.equalTo("UP"));
+    }
 }
