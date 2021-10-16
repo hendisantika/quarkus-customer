c65971cd4e3cea85e122eaf61ade1eec94af88aa
Hendi Santika <hendisantika@gmail.com>
2021-10-16 07:07:49 +0700

    #31 - Add HealthCheckTest

---
+++ src/test/java/com/hendisantika/HealthCheckTest.java
@@ -0,0 +1,16 @@
+package com.hendisantika;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-customer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/10/21
+ * Time: 07.07
+ */
+@QuarkusTest
+public class HealthCheckTest {
+}
